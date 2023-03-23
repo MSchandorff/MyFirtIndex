7678c0e3e8c3fa23c1ff157aa26150e6d85501cc
Mikkel <mkkl.schandorff@gmail.com>
2023-03-23 17:32:47 +0100

    Initial commit

---
+++ NolekStuff/ElasticClient.cs
@@ -0,0 +1,73 @@
+﻿using Elastic.Clients.Elasticsearch;
+using Elastic.Transport;
+
+namespace NolekStuff
+{
+    public class ElasticClient
+    {
+        private static readonly ElasticClient _instance = new ElasticClient();
+
+        private ElasticClient() {
+            if (_client == null)
+            {
+                var settings = new ElasticsearchClientSettings(new Uri("http://localhost:9200"));
+
+                _client = new ElasticsearchClient(settings);
+            }
+        }
+        
+
+        public static ElasticClient Instance { get { return _instance; } }
+
+        public ElasticsearchClient _client { get; set; }
+
+
+        public async void FullIndex()
+        {
+            var Dogs = new List<Dog>()
+            {
+                new Dog()
+                {
+                    Id = 1,
+                    Name = "Søren",
+                    Photo = new DogPhoto()
+                    {
+                        Id = 1,
+                        Url = "https://www.cdc.gov/healthypets/images/pets/cute-dog-headshot.jpg?_=42445",
+                        Description = "Very cute dog",
+                        Tags = new List<string>()
+                    {
+                        "New York",
+                        "Big Apple",
+                        "USA",
+                        "Tourist Dog"
+                    }
+        }
+                }
+            };
+
+            var response = _client.Index(Dogs, "dog-index");
+
+            if (response.IsValidResponse)
+            {
+                Console.WriteLine($"Index document with ID {response.Id} succeeded.");
+            }
+        }
+
+        public async void DocumentIndex()
+        {
+            var document = new TestDocument
+            {
+                StringProperty = "value123"
+            };
+
+            var Response = await _client
+                .IndexAsync(document, "my-document-index");
+
+            //if (Response.IsValidResponse)
+            //{
+            //    Console.WriteLine($"Index document with ID {indexResponse.Id} succeeded.");
+            //}
+        }
+    }
+}
---
+++ NolekStuff/TestDocument.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NolekStuff
+{
+    internal class TestDocument
+    {
+        public string StringProperty { get; set; }
+    }
+}
